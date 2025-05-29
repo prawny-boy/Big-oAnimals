ec77a850193c234256f35c182393a012cf5e4b0c
Sean Chan <sean.chan3@student.education.wa.edu.au>
2025-05-30 06:32:51 +0800

    Refactor signup state to enhance button management and improve user input validation

--- States/signup.py
+++ States/signup.py
@@ -7,18 +7,24 @@ from constants import *
 class SignupState(State):
     def __init__(self, game):
         super().__init__(game)
-        self.username_entry = TextBox("Username", 200, 210, 400, 40, 4, 20)
-        self.password_entry = TextBox("Password", 200, 260, 400, 40, 4, 20)
-        self.submit_button = Button(400, 420, 400, 100, "Submit")
+        self.username_entry = TextBox("Username", 200, 210, 400, 40, 4, 20, colour=YELLOW)
+        self.password_entry = TextBox("Password", 200, 260, 400, 40, 4, 20, colour=YELLOW)
+        self.buttons = {
+            "submit": Button(400, 420, 400, 100, "Submit"),
+            "back": Button(65, 570, 90, 45, "Back", border_width=3),
+            "next": Button(735, 570, 90, 45, "Next", True, border_width=3)
+        }
     
     def update(self, delta_time, actions):
         self.password_entry.update(actions, delta_time)
         self.username_entry.update(actions, delta_time)
         if actions["mouse_click"]:
-            if self.submit_button.check_hover():
+            if self.buttons["submit"].check_hover():
                 username = self.username_entry.get_text()
+                if not username:
+                    return
                 password = self.password_entry.get_text()
-                if not username or not password:
+                if not password:
                     return
                 elif username == password:
                     create_alert("Username and Password cannot be the same", 400, 200)
@@ -27,11 +33,14 @@ class SignupState(State):
                 else:
                     self.game.add_user(username)
                     HomeState(self.game).enter_state()
+            elif self.buttons["back"].check_hover():
+                self.exit_state()
     
     def render(self, surface):
         surface.fill(GREEN)
-        self.submit_button.draw(surface)
         self.password_entry.draw(surface)
         self.username_entry.draw(surface)
         alerts.update(surface)
+        for button in self.buttons.values():
+            button.draw(surface)
         draw_text("Signup Page", 400, 50, surface, YELLOW, HEADING_FONT)
